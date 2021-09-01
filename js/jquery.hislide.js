0a284f1aad20e973fd387703263c145a3a3e81cb
Mainbull <pixsolbulls@gmail.com>
2021-09-02 02:20:15 +0530

    Update jquery.hislide.js

--- js/jquery.hislide.js
+++ js/jquery.hislide.js
@@ -8,7 +8,7 @@
         // 默认设置选项
         var setting = {
         		// 控制轮播的动画时间
-            speed: 1500,
+            speed: 1000,
             // 控制 interval 的时间 (轮播速度)
             interval: 2000,
             
@@ -19,9 +19,9 @@
         var states = [
             { $zIndex: 1, width: 120, height: 150, top: 69, left: 134, $opacity: 1 },
             { $zIndex: 2, width: 130, height: 170, top: 59, left: 0, $opacity: 1},
-            { $zIndex: 3, width: 170, height: 218, top: 35, left: 110, $opacity: 1 },
+            { $zIndex: 3, width: 150, height: 200, top: 35, left: 110, $opacity: 1 },
             { $zIndex: 4, width: 300, height: 300, top: 0, left: 230, $opacity: 1 },
-            { $zIndex: 3, width: 170, height: 218, top: 35, left: 470, $opacity: 1 },
+            { $zIndex: 3, width: 150, height: 200, top: 35, left: 470, $opacity: 1 },
             { $zIndex: 2, width: 130, height: 170, top: 59, left: 620, $opacity: 1},
             { $zIndex: 1, width: 120, height: 150, top: 69, left: 500, $opacity: 1 }
         ];
